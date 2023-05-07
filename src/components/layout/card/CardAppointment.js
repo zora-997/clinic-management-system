4eb43d91e1ceb78138cc141efde2abf0d395665c
zorab rostam <zorabrostam315@gmail.com>
2023-05-06 18:08:50 -0700

    ckardni hmui

--- src/components/layout/card/CardAppointment.js
+++ src/components/layout/card/CardAppointment.js
@@ -3,63 +3,63 @@ import React from 'react'
 const CardAppointment = () => {
     return (
         <div className=' bg-white p-2 border-t border-t-gray-100 shadow-sm shadow-gray-200 mt-5 rounded-t-3xl '>
-            <table class="  w-full text-sm text-left text-gray-500 ">
-                <thead class="text-xs rounded-2xl  text-gray-900 uppercase ">
+            <table className="  w-full text-sm text-left text-gray-500 ">
+                <thead className="text-xs rounded-2xl  text-gray-900 uppercase ">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Name
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Gender
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Disease
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Time
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="bg-white">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap capitalize ">
+                    <tr className="bg-white">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-500 whitespace-nowrap capitalize ">
                             arkan osman
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             male
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             <span className='text-blue-500 bg-blue-100/60 rounded-3xl p-1 px-2'>shakara</span>
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             10:30 AM
                         </td>
                     </tr>
-                    <tr class="bg-white ">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap ">
+                    <tr className="bg-white ">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-500 whitespace-nowrap ">
                             zanyar jaza
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             male
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             <span className='text-green-500 bg-green-100/60 rounded-3xl p-1 px-2'> corona</span>
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             12:25 PM
                         </td>
                     </tr>
-                    <tr class="bg-white ">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap ">
+                    <tr className="bg-white ">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-500 whitespace-nowrap ">
                             kazhan
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             female
                         </td>
-                        <td class="px-6 py-4 ">
+                        <td className="px-6 py-4 ">
                             <span className='text-purple-500 bg-purple-100/60 rounded-3xl p-1 px-2'>hallamat</span>
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             3:15 AM
                         </td>
                     </tr>
